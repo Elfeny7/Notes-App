6fc9befd702bcdbcbb7ad8002cc6eeabc5c68e3d
elfeny7 <ikmalfaris50@gmail.com>
2023-10-23 20:15:40 +0700

    feat: delete note button, make generic error dialog

---
+++ lib/utilities/dialogs/logout_dialog.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:september_app/utilities/dialogs/generic_dialog.dart';
+
+Future<bool> showLogOutDialog(
+  BuildContext context,
+) {
+  return showGenericDialog<bool>(
+    context: context,
+    title: 'Logout',
+    content: 'Are you sure to logout?',
+    optionsBuilder: () => {
+      'Cancel': false,
+      'Log out': true,
+    },
+  ).then(
+    (value) => value ?? false,
+  );
+}
