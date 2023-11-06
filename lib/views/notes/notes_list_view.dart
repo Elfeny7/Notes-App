90d2fc4f9dd8d26e8248ec1c6776c1c791956024
elfeny7 <ikmalfaris50@gmail.com>
2023-11-06 22:14:31 +0700

    fix: rename DeleteNoteCallback to NoteCallback

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:september_app/services/crud/notes_service.dart';
 import 'package:september_app/utilities/dialogs/delete_dialog.dart';
 
-typedef DeleteNoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(DatabaseNote note);
 
 class NotesListView extends StatelessWidget {
   final List<DatabaseNote> notes;
-  final DeleteNoteCallback onDeleteNote;
+  final NoteCallback onDeleteNote;
 
   const NotesListView({
     super.key,
